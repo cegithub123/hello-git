7469eeeefc95e4e852bfae846517ccab68a4edd7
Cesar <ce.nino@gmail.com>
2025-11-03 12:37:58 -0500

    merge mezclando

--- helloGit.py
+++ helloGit.py
@@ -1,17 +1,17 @@
 print ("hello git, cambiando todo en este archivo")
 
-print("linea1 hecha en main")
+print("linea1 hecha en main111")
 print("linea2 hecha en main")
-print("linea3 hecha en main")
+print("linea3 hecha en main222")
 print("linea4 hecha en main")
 
 print("linea1 hecha en rama")
 print("linea2 hecha en rama")
-print("linea3 hecha en rama")
+print("linea3 hecha en rama333333")
 print("linea4 hecha en rama")
 print("linea5 hecha en rama")
 
 
 print("lo nuevo 1 en rama")
-print("lo nuevo 2 en rama")
+print("lo nuevo 2 en rama4444444")
 print("lo nuevo 3 en rama")
