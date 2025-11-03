ff4d179b7e4092f1b04af9bd171ca5f51d5c414d
Cesar <ce.nino@gmail.com>
2025-11-03 12:30:11 -0500

    lineas en rama

--- helloGit.py
+++ helloGit.py
@@ -1,1 +1,7 @@
 print ("hello git, cambiando todo en este archivo")
+
+print("linea1 hecha en rama")
+print("linea2 hecha en rama")
+print("linea3 hecha en rama")
+print("linea4 hecha en rama")
+print("linea5 hecha en rama")
